ec88cb441070710cbde29cb899a424db934cf09f
Adam Gribble <adam@thehumangeo.com>
2018-09-23 15:19:41 +0100

    Updated the hero banner

--- app/components/adm-hero-banner/component.js
+++ app/components/adm-hero-banner/component.js
@@ -1,15 +1,9 @@
-import { computed } from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['flex', 'flex-col'],
-  // classNames: ['layout-column', 'layout-align-gt-sm-center-center', 'layout-align-center'],
 
   name: '',
-  tagline: '',
-  email: '',
-
-  taglineParts: computed('tagline', function() {
-    return this.tagline.split('.').map(item => item ? `${item}. ` : '');
-  })
+  taglineParts: '',
+  email: ''
 });
--- app/profile/controller.js
+++ app/profile/controller.js
@@ -7,6 +7,7 @@ export default Controller.extend({
     this.setProperties({
       languages: ['JavaScript (ES6)', 'Python', 'SQL'],
       frameworks: ['EmberJS', 'SailsJS', 'ExpressJS', 'Django', 'AngularJS', 'Flask', 'LeafletJS', 'jQuery'],
+      taglineParts: ['Full-Stack Engineer', 'Avid Traveler', 'Home Cook'],
       tools: ['ElasticSearch', 'MongoDB', 'MySQL', 'Redis'],
       web: ['HTML', 'CSS', 'LESS', 'SASS', 'Material Design', 'Twitter Bootstrap']
     });
