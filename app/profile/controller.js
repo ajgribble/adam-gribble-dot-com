14a7a725722e759149f3404c50c896966d4f653d
Adam Gribble <adam@thehumangeo.com>
2018-10-13 16:23:59 +0100

    Moved hard-coded values to ENV variables

--- app/profile/controller.js
+++ app/profile/controller.js
@@ -1,15 +1,17 @@
 import Controller from '@ember/controller';
+import ENV from 'adam-gribble-dot-com/config/environment';
+
+const { profileEmail, profileName, profileSkills, tagline } = ENV;
 
 export default Controller.extend({
   init() {
     this._super();
 
     this.setProperties({
-      languages: ['JavaScript (ES6)', 'Python', 'SQL'],
-      frameworks: ['EmberJS', 'SailsJS', 'ExpressJS', 'Django', 'AngularJS', 'Flask', 'LeafletJS', 'jQuery'],
-      taglineParts: ['Full-Stack Engineer', 'Avid Traveler', 'Home Cook'],
-      tools: ['ElasticSearch', 'MongoDB', 'MySQL', 'Redis'],
-      web: ['HTML', 'CSS', 'LESS', 'SASS', 'Material Design', 'Twitter Bootstrap']
+      email: profileEmail,
+      name: profileName,
+      skills: profileSkills,
+      tagline
     });
   }
 });
