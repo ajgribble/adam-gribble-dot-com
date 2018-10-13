4f8520cc7c570a50a013a07018b05a80d7ef7e97
Adam Gribble <adam@thehumangeo.com>
2018-10-13 15:28:56 +0100

    Completed the refactored design with tailwind.css

--- app/components/adm-checklist-container/component.js
+++ app/components/adm-checklist-container/component.js
@@ -2,12 +2,15 @@ import { computed } from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['flex-1', 'text-white'],
-  classNameBindings: ['bgColor'],
+  classNames: ['flex-1', 'p-2'],
+  classNameBindings: ['bgColor', 'borderColor'],
 
   color: 'grey',
 
   bgColor: computed('color', function() {
     return `bg-${this.color}`;
+  }),
+  borderColor: computed('color', function() {
+    return `border-${this.color}`
   })
 });
