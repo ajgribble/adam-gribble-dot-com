17d0937828517d97a8ca8cf1ceb8296054fc0c21
adam.j.gribble <adam.j.gribble.civ@mail.mil>
2019-04-13 19:26:28 +0100

    Wired up an engine

--- app/router.js
+++ app/router.js
@@ -8,6 +8,11 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('profile', { path: '/' });
+
+  this.mount('sample-dashboard', {
+    resetNamespace: true,
+    path: 'sample-dashboard'
+  });
 });
 
 export default Router;
